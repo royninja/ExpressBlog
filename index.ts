2bf4c4d1276fd1d7ffb2766f76a07020f50b4cf5
royninja <sayan98.roy@gmail.com>
2024-06-24 22:36:18 +0530

    add index with boostrap

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import express,{Express,Request,Response} from "express";
+import path from "path";
 
 const app : Express = express();
 const port = 9000;
 
 app.get("/",(req : Request,res : Response) =>{
-    res.send("<h1>Home</h1>");
+    //res.send("<h1>Home</h1>");
+    res.sendFile(path.join(__dirname,"../index.html"));
+    
 });
 
 app.get("/about",(req : Request,res : Response) =>{
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 9000;
 app.get("/", (req, res) => {
-    res.send("<h1>Home</h1>");
+    //res.send("<h1>Home</h1>");
+    res.sendFile(path_1.default.join(__dirname, "../index.html"));
 });
 app.get("/about", (req, res) => {
     res.send("<h2>About</h2>");
